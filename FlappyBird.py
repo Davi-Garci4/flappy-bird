ee31e7e5302c6e529ce6e7a0b675b9943af6d4d4
Davi-Garci4 <davi.garcia@aluno.faculdadeimpacta.com.br>
2023-08-07 16:51:06 -0300

    Criei o objeto do pássaro, a adicionei alguns comentários

--- FlappyBird.py
+++ FlappyBird.py
@@ -11,3 +11,30 @@ imagem_background = pygame.transform.scale2x(pygame.image.load(os.path.join('ima
 imagens_passaro = [pygame.transform.scale2x(pygame.image.load(os.path.join('imagens', 'bird1.png'))),
                    pygame.transform.scale2x(pygame.image.load(os.path.join('imagens', 'bird2.png'))),
                    pygame.transform.scale2x(pygame.image.load(os.path.join('imagens', 'bird3.png')))]
+
+pygame.font.init()               #Aqui eu estou inicializando a entrada do texto atravéz da pygame
+fonte_pontos = pygame.font.SysFont("arial", 50)     #Aqui esta sendo informada a fonte e o tamanho dela
+
+class Passaro:
+    imagens = imagens_passaro
+    #animação de rotação
+    rotacao_maxima = 25
+    velocidade_rotacao = 20
+    tempo_animacao = 5
+
+    def __init__(self, x ,y):
+        self.x = x
+        self.y = y
+        self.angulo = 0          #Aqui eu estou criando o angulo do pássaro, considerando o grau de inclinação que ele está
+        self.velocidade = 0      #Aqui é a velocidade em que ele se movimenta, no caso pra cima e pra baixo
+        self.altura = self.y     #Aqui é a altura em que ele se encontra, considerando que a altura dele é controlada pelo usuário apartir do inicio do jogo
+        self.tempo = 0           #Aqui é o tempo do grau de inclinação dele
+        self.contagem_imagem = 0  #Aqui é para informar qual imagem do passaro eu estou utilizando no momento
+        self.imagem = imagens[0]  #Aqui é a imagem padrão/inicial que aparece do pássaro
+
+
+class Cano:
+    pass
+
+class Chao:
+    pass
